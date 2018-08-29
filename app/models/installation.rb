6eb3f9d8bff780da618f1566db1b1ec0a52f72dd
Emba Moussa <emba.moussa@eventinc.de>
2018-08-29 14:01:36 +0200

    feat: basic tdd for Installation new fields

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,5 +1,16 @@
 class Installation < ActiveRecord::Base
   delegate :last_commit, :lines_diff, :files_changed, :diff_url, to: :github
+  enum organization_type: {
+    government: 'government',
+    ong: 'ong',
+    university: 'university',
+    school: 'school',
+    other: 'other',
+  }
+  enum installation_status: {
+    in_progress: 'in_progress',
+    in_production: 'in_production',
+  }
 
   def github
     Installation::Github.new(self)
---
+++ spec/models/installation_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Installation, type: :model do
+
+  it { is_expected.to respond_to(:name) }
+  it { is_expected.to respond_to(:repo) }
+  it { is_expected.to respond_to(:created_at) }
+  it { is_expected.to respond_to(:updated_at) }
+  it { is_expected.to respond_to(:website) }
+  it { is_expected.to respond_to(:contact_name) }
+  it { is_expected.to respond_to(:conact_email) }
+  it { is_expected.to respond_to(:location) }
+  it { is_expected.to respond_to(:organization_type) }
+  it { is_expected.to respond_to(:status) }
+  it { is_expected.to respond_to(:notes) }
+
+  it "is valid with valid attributes" do
+    expect(Installation.new).to be_valid
+  end
+
+end
