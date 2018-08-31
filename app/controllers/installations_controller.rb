05d72457c78fca2e3634d423d7936c737f051f59
Emba Moussa <emba.moussa@eventinc.de>
2018-08-31 14:45:40 +0200

    feat: installations controller, create action with tdd

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -12,4 +12,23 @@ class InstallationsController < ApplicationController
     @installation = Installation.new
   end
 
+  def create
+    @installation = Installation.new(installation_params)
+
+    respond_to do |format|
+      if @installation.save
+        format.html { redirect_to @installation, notice: 'installation was successfully created.' }
+        format.json { render :show, status: :created, location: @installation }
+      else
+        format.html { render :new }
+        format.json { render json: @installation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def installation_params
+      params.require(:installation).permit(:name, :repo, :website, :contact_name, :conact_email, :location, :organization_type, :status, :notes)
+    end
+
 end
--- spec/controllers/installations_controller_spec.rb
+++ spec/controllers/installations_controller_spec.rb
@@ -26,4 +26,40 @@ RSpec.describe InstallationsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    let(:installation_params) {
+      {
+        name: Faker::Lorem.sentence,
+        repo: Faker::Internet.url('github.com'),
+        website: Faker::Internet.url,
+        contact_name: Faker::Name.name_with_middle,
+        conact_email: Faker::Internet.email,
+        location: Faker::Address.full_address,
+        organization_type: Installation.organization_types.keys.sample,
+        status: Installation.installation_statuses.keys.sample,
+        notes: Faker::Lorem.paragraph,
+      }
+    }
+
+    context "with valid params" do
+      it "creates a new Installation" do
+        expect {
+          post :create, params: {installation: installation_params}, session: valid_session
+        }.to change(Installation, :count).by(1)
+      end
+
+      it "redirects to the created installation" do
+        post :create, params: {installation: installation_params}, session: valid_session
+        expect(response).to redirect_to(Installation.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "returns a success response (i.e. to display the 'new' template)" do
+        post :create, params: {installation: {foo: 'foo', bar: 'bar'}}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
+  end
+
 end
